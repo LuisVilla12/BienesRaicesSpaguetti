98d9b2b67d16974c55ac62bb59ea9c84132fe7c0
Luis Villa <luisji2312ia@gmail.com>
2022-01-23 10:56:23 -0600

    v18 JS Btn Dark

--- build/javas.js
+++ build/javas.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     eventListeners();
+    darkMode();
 });
 
 function eventListeners() {
     const menu = document.querySelector('.mobil_menu');
+    // Cuando se da click se invoca la funcion de navegacion
     menu.addEventListener('click', navegacion)
 }
 
@@ -11,5 +13,14 @@ function navegacion() {
     const nav = document.querySelector('.nav');
     // Si la tiene la quita sino la elimina
     nav.classList.toggle('mostrar');
+}
 
+function darkMode() {
+    const btnDark = document.querySelector('.dark-mode-btn');
+    btnDark.addEventListener('click', dark)
+}
+
+function dark() {
+    // Añade la clase de dark-mode a todo el html
+    document.body.classList.toggle('dark-mode');
 }
