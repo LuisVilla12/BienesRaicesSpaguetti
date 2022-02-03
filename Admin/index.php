d902176dedab21ffd9581d0d3ee143748ed9337d
Luis Villa <luisji2312ia@gmail.com>
2022-02-03 00:22:09 -0600

    v15 Eliminando propieades

--- Admin/index.php
+++ Admin/index.php
@@ -3,11 +3,28 @@
 require '../includes/config/database.php';
 $db = conectarDB();
 // Query
-$queryPropiedades="SELECT * FROM propiedades";
+$queryPropiedades = "SELECT * FROM propiedades";
 //Consultar la base de datos
-$resultadoPropiedades=mysqli_query($db,$queryPropiedades);
+$resultadoPropiedades = mysqli_query($db, $queryPropiedades);
 // Sino existe se asigna el valor null
 $registro = $_GET['registro'] ?? null;
+
+// Obtiene el valor del metodo post del input hidden
+if($_SERVER['REQUEST_METHOD']==='POST'){
+    $id=$_POST['id']; 
+    $id=filter_var($id,FILTER_VALIDATE_INT);
+    if($id){
+        // Eliminar imagen con ese ID
+        $queryImagen = "SELECT imagen FROM propiedades WHERE id=${id}";
+        $resultadoImagen=mysqli_query($db,$queryImagen);
+        // Eliminar el registro con ese ID
+        $queryDelete="DELETE FROM propiedades WHERE id=${id}";
+        $resultadoDelete=mysqli_query($db,$queryDelete);
+        if($resultadoDelete){
+            header('Location:/admin?registro=3');
+        }
+    }
+}
 // Importa las funciones
 require '../includes/funciones.php';
 incluirTemplate('header');
@@ -17,11 +34,16 @@ incluirTemplate('header');
     <?php if (intval($registro) === 1) : ?>
         <div class="alerta exito">
             <p>Propiedad registrada correctamento</p>
-        </div>    
+        </div>
     <?php endif; ?>
     <?php if (intval($registro) === 2) : ?>
         <div class="alerta exito">
             <p>Propiedad actualizada correctamento</p>
+        </div>
+    <?php endif; ?>
+    <?php if (intval($registro) === 3) : ?>
+        <div class="alerta exito">
+            <p>Propiedad eliminada correctamento</p>
         </div>
     <?php endif; ?>
     <a href="/Admin/propiedades/crear.php" class="btn">Añadir propiedad</a>
@@ -36,25 +58,28 @@ incluirTemplate('header');
             </tr>
         </thead>
         <tbody>
-            <?php while ($propiedad=mysqli_fetch_assoc($resultadoPropiedades)): ?>
-            <tr>
-                <td><?php echo $propiedad['id'] ?></td>
-                <td><?php echo $propiedad['titulo'] ?></td>
-                <td>
-                    <img src="/imagenes/<?php echo $propiedad['imagen'] ?>" alt="l">
-                </td>
-                <td>$<?php echo $propiedad['precio'] ?></td>
-                <td>
-                    <a href="/Admin/propiedades/actualizar.php?id=<?php echo $propiedad['id'];?>" class="btn amarillo" >Actualizar</a>
-                    <a href="" class="btn rojo" >Eliminar</a>
-                </td>
-            </tr>
-            <?php endwhile; ?> 
+            <?php while ($propiedad = mysqli_fetch_assoc($resultadoPropiedades)) : ?>
+                <tr>
+                    <td><?php echo $propiedad['id'] ?></td>
+                    <td><?php echo $propiedad['titulo'] ?></td>
+                    <td>
+                        <img src="/imagenes/<?php echo $propiedad['imagen'] ?>" alt="l">
+                    </td>
+                    <td>$<?php echo $propiedad['precio'] ?></td>
+                    <td>
+                        <a href="/Admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="btn amarillo">Actualizar</a>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
+                            <input type="submit" class="btn rojo" value="Eliminar">
+                        </form>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
         </tbody>
     </table>
 </main>
 
-<?php 
+<?php
 mysqli_close($db);
-incluirTemplate('footer'); 
+incluirTemplate('footer');
 ?>
